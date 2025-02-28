c936c38866540f488a1645f9b6eb43cc2bf2fe50
Ana Julia <anajulia@email.com>
2025-02-28 11:06:46 -0300

    Término da Fase 3 -Implementado endpoint FindByCityIgnoreCase. -Update AirportRepository - Adicionado assinatura de método FindByIgnoreCase; -Update AirportService - Adicionado método FindByCity -Update AirportController - Adicionado endpoint /city/{cityName}

--- src/main/java/dev/anajulia/br/airports/controllers/AirportController.java
+++ src/main/java/dev/anajulia/br/airports/controllers/AirportController.java
@@ -8,7 +8,9 @@ import dev.anajulia.br.airports.entities.Airport;
 import dev.anajulia.br.airports.services.AirportService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -26,4 +28,25 @@ public class AirportController {
         List<Airport> result = airportService.findAll();
         return result;
     }
+   
+ @GetMapping ("/city/{cityName}")
+ public ResponseEntity<List<Airport>> FindByCityIgnoreCase(@PathVariable String cityName){
+     List<Airport> result = airportService.findByCity(cityName);
+     
+     if (result.isEmpty()){
+         //Ops.. lista vazia...
+         //notFound devolve 404
+         return ResponseEntity.notFound().build();
+         
+     } else {
+         // Eba! Tem dados!
+         // ok devolve 200
+         return ResponseEntity.ok(result);
+     }
+     
+ 
+ }
+    
+
+ 
 }
--- src/main/java/dev/anajulia/br/airports/services/AirportService.java
+++ src/main/java/dev/anajulia/br/airports/services/AirportService.java
@@ -17,7 +17,14 @@ public class AirportService {
         
         List<Airport> result = airportRepository.findAll();
         return result;
-       
+  
     }
-    
+        
+    public List<Airport> findByCity(String city){
+        List<Airport> result = airportRepository.findByCityIgnoreCase(city);
+        return result;
+   
+
+ 
+   }
 }
